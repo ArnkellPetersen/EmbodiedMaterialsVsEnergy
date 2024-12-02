209d64fced9f8b279791aa9b34c524576ecedb13
ArnkellPetersen <arnkell.jonas.petersen@nmbu.no>
2024-12-02 20:03:33 +0100

    evolution baby

--- src/GWP_functions.py
+++ src/GWP_functions.py
@@ -13,3 +13,16 @@ def temporal_adjustment(t, T=100):
         float : Temporal adjustment factor
     """
     return 2 - np.exp((np.log(2)/T)*t)
+
+def temporal_adjustment_probable(t, p=1, T=100):
+    """Temporal adjustment according to Reisch
+
+    Args:
+        t (float): year of calculated temperoral adjustment
+        p (float): probability of usage. Defaults to 1.
+        T (int, optional): period of consideration. Defaults to 100.
+
+    Returns:
+        float : Temporal adjustment factor
+    """
+    return 2 - np.exp((np.log(2)/T)*t)*p
